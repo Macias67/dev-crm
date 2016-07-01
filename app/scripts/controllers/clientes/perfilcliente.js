3cdac3cda5cbc8570eaefec911b2b59c2e6f1889
Luis Macias <luismacias.angulo@gmail.com>
2016-06-30 20:42:22 -0500

    vista perfil del cliente

--- app/scripts/controllers/clientes/perfilcliente.js
+++ app/scripts/controllers/clientes/perfilcliente.js
@@ -9,22 +9,23 @@
  */
 angular.module('MetronicApp')
 	.controller('PerfilClienteCtrl', [
-		'$rootScope', '$scope', 'dataCliente',
-		function ($rootScope, $scope, dataCliente) {
-			var vm      = this;
+		'$rootScope', '$scope', 'dataCliente', '$window',
+		function ($rootScope, $scope, dataCliente, $window) {
+			var vm     = this;
 			vm.cliente = {};
-
 			dataCliente.$promise.then(function (d) {
 				vm.cliente = d.data;
-				console.log(d.data);
-
 				setTimeout(function () {
 					App.unblockUI('#ui-view');
 				}, 2000);
 			});
-
-
-
+			
+			vm.contactos = {
+				modalNuevoContacto: function () {
+					$window.alert('Ola k aze?');
+				}
+			};
+			
 			// set sidebar closed and body solid layout mode
 			$rootScope.settings.layout.pageContentWhite  = false;
 			$rootScope.settings.layout.pageBodySolid     = false;
@@ -35,10 +36,10 @@ angular.module('MetronicApp')
 				App.initAjax();
 				App.scrollTop();
 				App.blockUI({
-					target      : '#ui-view',
-					message     : '<b> Cargando datos del cliente </b>',
-					boxed       : true,
-					zIndex      : 99999,
+					target    : '#ui-view',
+					message   : '<b> Cargando datos del cliente </b>',
+					boxed     : true,
+					zIndex    : 99999,
 					overlayCSS: {
 						backgroundColor: App.getBrandColor('grey'),
 						opacity        : 0.4,
